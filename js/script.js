7039a333d93bb801e9f6a576a1edcb69ceb76997
Junior Andrade <115849218+WJuniorAndrade@users.noreply.github.com>
2023-04-30 20:24:30 -0300

    Versão Desktop
    Criado um layout HTML, CSS e com Java Script, onde ao ser selecionada a moeda uma imagem de fundo correspondente a moeda seja exibida.

--- js/script.js
+++ js/script.js
@@ -4,12 +4,9 @@ for (let i = 0; i < moedas.length; i++) {
     var moeda = moedas[i]
     moeda.addEventListener("click", function() {
 
-        var valorInput = parseInt( document.querySelector("#valor").value)
+        var valorInput = parseInt( document.querySelector("#valor").value);
 
-        var para = document.querySelector("#para")
-        para.textContent= ` Sua moeda é: ${moedas[i].value}`
-
-        console.log(moedas[i].value);
+        console.log(`Moeda selecionada: ${moedas[i].value}`);
 
         validarMoeda(moedas[i].value, valorInput)
     });
--- js/validate.js
+++ js/validate.js
@@ -1,5 +1,5 @@
-function validarMoeda(moedas,valorInput) {
-    
+function validarMoeda(moedas, valorInput) {
+
     var dolarAmericano = document.getElementById("dolar-americano").value;
     var dolarCanadiano = document.getElementById("dolar-canadiano").value;
     var dolarAustraliano = document.getElementById("dolar-australiano").value;
@@ -7,26 +7,65 @@ function validarMoeda(moedas,valorInput) {
     var euro = document.getElementById("euro").value;
     var bitcoin = document.getElementById("bitcoin").value;
 
+    var painelConversor = document.getElementById("painel-conversor")
+    var bandeiraFundo = document.querySelector(".bandeira-background")
+
+
     if (moedas == dolarAmericano) {
         console.log(valorInput);
-        /* function converterDolarAmericano(painelConversor, valorInput)  */
+        converterDolarAmericano(painelConversor, valorInput)
+        bandeiraFundo.classList.add("america")
+    } 
+    else {
+        bandeiraFundo.classList.remove("america")
     }
 
+
     if (moedas == dolarCanadiano) {
-        
+        console.log(valorInput);
+        converterDolarCanadiano(painelConversor, valorInput)
+        bandeiraFundo.classList.add("canadar")
     }
+    else {
+        bandeiraFundo.classList.remove("canadar")
+    }
+
+
     if (moedas == dolarAustraliano) {
-        
+        console.log(valorInput);
+        converterDolarAustraliano(painelConversor, valorInput)
+        bandeiraFundo.classList.add("australia")
+    }
+    else {
+        bandeiraFundo.classList.remove("australia")
     }
 
     if (moedas == IeneJapones) {
-        
+        console.log(valorInput);
+        converterIeneJapones(painelConversor, valorInput)
+        bandeiraFundo.classList.add("japao")
     }
-    if (moedas == euro) {
-        
+    else {
+        bandeiraFundo.classList.remove("japao")
     }
 
+
+    if (moedas == euro) {
+        console.log(valorInput);
+        converterEuro(painelConversor, valorInput)
+        bandeiraFundo.classList.add("europa")
+    }
+    else {
+        bandeiraFundo.classList.remove("europa")
+    }
+
+
     if (moedas == bitcoin) {
-        
+        console.log(valorInput);
+        converterBtiCoin(painelConversor, valorInput)
+        bandeiraFundo.classList.add("bitcoin")
+    }
+    else {
+        bandeiraFundo.classList.remove("bitcoin")
     }
 }
